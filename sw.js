7d111c67dcbea2a890c3d3e9390b357b1cf8435a
simpsons-generator <gadsegaderr@gmail.com>
2025-07-31 14:29:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
 var GHPATH = 'https://simpsons-generator.github.io/test';
-var APP_PREFIX = 'com.molch.simpgen';
-var VERSION = 'version_002';
+var APP_PREFIX = 'simp.gen';
+var VERSION = 'version_003';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
   `${GHPATH}/sw.js`,
   `${GHPATH}/manifest.webmanifest`,
   `${GHPATH}/images.png`,
+  `${GHPATH}/style.css
+  `${GHPATH}/script.js
 ]
